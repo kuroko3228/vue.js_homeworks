fd18d3064b83c3eb7d5535d467db8b5ff2a1e51d
Ешенкулов Олжас Ермекович <EshenkulovO@lcoast.itstep>
2023-10-31 18:39:50 +0600

    HW3-task1

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,83 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="block">
+    <div v-for="n in 3" :key="n" class="container">
+      <input v-model="newCharts[n].name" placeholder="Название" class="input_one">
+      <input type="number" v-model="newCharts[n].number" placeholder="Проценты" max="100" class="input_two">
+      <input v-model="newCharts[n].color" placeholder="Пиши на англ цвет" class="input-three">
+      <button @click="save_btn(n)" class="save_btn">Сохранить</button>
+      <div class="chart">
+        <div :style="{height: charts[n].number + '%', backgroundColor: charts[n].color}"></div>
+      </div>
+      <div class="chart-label">
+        {{ charts[n].name }}
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      charts: {
+        1: { name: '', number: 0, color: 'yellow' },
+        2: { name: '', number: 0, color: 'yellow' },
+        3: { name: '', number: 0, color: 'yellow' }
+      },
+      newCharts: {
+        1: { name: '', number: 0, color: '' },
+        2: { name: '', number: 0, color: '' },
+        3: { name: '', number: 0, color: '' }
+      }
+    };
+  },
+  methods: {
+    save_btn(id) {
+      this.charts[id].name = this.newCharts[id].name;
+      this.charts[id].number = Math.min(this.newCharts[id].number, 100);
+      this.charts[id].color = this.newCharts[id].color || 'yellow';
+    }
+  }
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.block {
+  display: flex;
+  justify-content: space-between;
+  max-width: 960px;
+  margin: 20px auto;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
+.container {
+  width: 300px;
   display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  padding: 20px;
+  border: 1px solid #000;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
-
-nav a:first-of-type {
-  border: 0;
+.input_one, .input_two, .input-three {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+  border: 1px solid #000;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.save_btn {
+  width: 100%;
+  padding: 10px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  margin-bottom: 10px;
+}
+.chart {
+  height: 300px;
+  border-top: 1px solid #000;
+  position: relative;
+}
+.chart-label {
+  text-align: center;
+  padding-top: 10px;
 }
 </style>
